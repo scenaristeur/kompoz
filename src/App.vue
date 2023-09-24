234733de1d80a9d04d57a34559dd9ca2f27e555f
scenaristeur <scenaristeur@gmail.com>
2023-09-24 14:45:26 +0200

    pages

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,17 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Kompoz" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/dekompoz">DeKompoz</RouterLink>
+        <RouterLink to="/rekompoz">ReKompoz</RouterLink>
+        <RouterLink to="/adokompoz">AdoKompoz</RouterLink>
+        <RouterLink to="/minikompoz">MiniKompoz</RouterLink>
+        <RouterLink to="/about">A propos</RouterLink>
+        <hr>
+        <RouterLink to="/dekompoz">Ressources</RouterLink> | <RouterLink to="/dekompoz">Temoignages</RouterLink>...
       </nav>
     </div>
   </header>
